3d710019e2ef287e921613191b5e83f2c3d863dd
ERWS <emerson@ew.org.nz>
2016-11-20 11:27:31 +1300

    Initial application project build

---
+++ diyPacletProject/PacletInfo.m
@@ -0,0 +1,14 @@
+(* Paclet Info File *)
+
+(* created 2016.11.20*)
+
+Paclet[
+  Name -> "diyPaclet",
+  Version -> "0.0.1",
+  Loading -> "Automatic",
+  MathematicaVersion -> "7+",
+  Root -> "diyPaclet",
+  Extensions -> {
+    {"Kernel",  Context -> "diyPaclet`"},
+    {"Documentation", Language -> "English"}
+}]
---
+++ diyPacletProject/diyPaclet/Kernel/diyPaclet.m
@@ -0,0 +1,16 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench 20/11/2016 *)
+
+BeginPackage["diyPaclet`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
