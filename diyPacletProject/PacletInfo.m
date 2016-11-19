bc4b364758f3bf6ea8e0972605a2f94901f2ff2a
ERWS <emerson@ew.org.nz>
2016-11-20 11:47:50 +1300

    Function f added along with documentation

--- diyPacletProject/PacletInfo.m
+++ diyPacletProject/PacletInfo.m
@@ -1,14 +1,18 @@
 (* Paclet Info File *)
 
-(* created 2016.11.20*)
+(* created 2016/11/20*)
 
 Paclet[
-  Name -> "diyPaclet",
-  Version -> "0.0.1",
-  Loading -> "Automatic",
-  MathematicaVersion -> "7+",
-  Root -> "diyPaclet",
-  Extensions -> {
-    {"Kernel",  Context -> "diyPaclet`"},
-    {"Documentation", Language -> "English"}
-}]
+    Name -> "diyPaclet",
+    Version -> "0.0.1",
+    MathematicaVersion -> "7+",
+    Loading -> "Automatic",
+    Root -> "diyPaclet",
+    Extensions -> 
+        {
+            {"Kernel", Context -> "diyPaclet`"}, 
+            {"Documentation", Language -> "English", MainPage -> "Guides/diyPacletGuide"}
+        }
+]
+
+
--- diyPacletProject/diyPaclet/Kernel/diyPaclet.m
+++ diyPacletProject/diyPaclet/Kernel/diyPaclet.m
@@ -3,12 +3,14 @@
 (* Created by the Wolfram Workbench 20/11/2016 *)
 
 BeginPackage["diyPaclet`"]
+
+f::usage = "f  "
 (* Exported symbols added here with SymbolName::usage *) 
 
 Begin["`Private`"]
 (* Implementation of the package *)
 
-
+f[x_]:=x^2
 
 End[]
 
