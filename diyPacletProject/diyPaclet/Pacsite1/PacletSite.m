b95cec0b529a5c8884f07528aae1dd2a4937b737
ERWS <emerson@ew.org.nz>
2016-11-26 18:53:57 +1300

    Paclet Site Files

---
+++ diyPacletProject/diyPaclet/Pacsite1/PacletSite.m
@@ -0,0 +1,1 @@
+PacletSite[Paclet["Name" -> "diyPaclet", "Version" -> "0.0.2", "MathematicaVersion" -> "7+", "Creator" -> "ERWS", "Loading" -> "Automatic", "Root" -> "diyPaclet", "Extensions" -> {{"Kernel", "Context" -> "diyPaclet`"}, {"Documentation", "Language" -> "English", "MainPage" -> "Guides/diyPacletGuide"}}, "Location" -> "https://github.com/ERWS/diyPaclet/releases/download/v1.0/"]]
